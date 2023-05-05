323218973314c6fd53da09aecaaac8972c7055ae
LanNetwork <ianmcgeoy@gmail.com>
2023-05-05 15:28:45 -0700

    Fixed urls so no path is routed to homepage view

--- hackathon_todolist/homepage/urls.py
+++ hackathon_todolist/homepage/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('homepage/', views.showList, name='homepage'),
+    # path('homepage/', views.showList),
+    path('', views.showList, name='homepage'),
+
 ]
